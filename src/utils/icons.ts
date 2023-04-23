f5d3d5aaeb79f462a16010bc104ba10729e7ae51
Filip Tibell <filip.tibell@gmail.com>
2023-04-23 08:35:11 +0200

    Default to instance icon for any unknown classes

--- src/utils/icons.ts
+++ src/utils/icons.ts
@@ -1,5 +1,47 @@
 import * as path from "path";
 
+const KNOWN_ICON_CLASSES: Set<string> = new Set([
+	"BasePart",
+	"DataModel",
+	"Folder",
+	"Instance",
+	"Lighting",
+	"LocalScript",
+	"LuaSourceContainer",
+	"MaterialService",
+	"MaterialVariant",
+	"MeshPart",
+	"Model",
+	"ModuleScript",
+	"Part",
+	"PlayerGui",
+	"Players",
+	"PlayerScripts",
+	"RemoteEvent",
+	"RemoteFunction",
+	"ReplicatedFirst",
+	"ReplicatedStorage",
+	"RunService",
+	"Script",
+	"ServerScriptService",
+	"ServerStorage",
+	"SoundService",
+	"StarterCharacterScripts",
+	"StarterGui",
+	"StarterPack",
+	"StarterPlayer",
+	"StarterPlayerScripts",
+	"Teams",
+	"Terrain",
+	"VoiceChatService",
+	"Workspace",
+	"WorldModel",
+]);
+
 export const getClassIconPath = (className: string): string => {
-	return path.join(__dirname, "..", "..", "icons", `${className}.png`);
+	if (KNOWN_ICON_CLASSES.has(className)) {
+		return path.join(__dirname, "..", "..", "icons", `${className}.png`);
+	} else {
+		return path.join(__dirname, "..", "..", "icons", `Instance.png`);
+	}
 };
