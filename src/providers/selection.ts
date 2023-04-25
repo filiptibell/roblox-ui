2d6d4766d1fc2e9f6b8ca696cc1370945417dee1
Filip Tibell <filip.tibell@gmail.com>
2023-04-25 08:46:46 +0200

    Focus instances in the explorer when their text editor becomes visible

--- src/providers/selection.ts
+++ src/providers/selection.ts
@@ -9,6 +9,29 @@ export class SelectionProvider implements vscode.Disposable {
 		treeView: vscode.TreeView<vscode.TreeItem>,
 		treeDataProvider: RojoTreeProvider
 	) {
+		// Reveal instances in the explorer when editors for them become visible
+		let visibleEditors: Map<string, vscode.TextEditor> = new Map();
+		this.disposables.push(
+			vscode.window.onDidChangeVisibleTextEditors((editors) => {
+				const newEditors = new Map();
+				for (const editor of editors) {
+					const editorPath = editor.document.uri.fsPath;
+					newEditors.set(editorPath, editor);
+					if (!visibleEditors.has(editorPath)) {
+						const treeItem = treeDataProvider.find(editorPath);
+						if (treeItem) {
+							treeView.reveal(treeItem, {
+								select: true,
+								focus: true,
+							});
+						}
+					}
+				}
+				visibleEditors = newEditors;
+			})
+		);
+
+		// Update actions context (copy, paste, insert) when the explorer selection changes
 		this.disposables.push(
 			treeView.onDidChangeSelection((event) => {
 				const selected: Array<any> = Array.from(event.selection);
