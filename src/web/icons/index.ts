fe20cc16e0d8d383de9209464fdfa62963378570
Filip Tibell <filip.tibell@gmail.com>
2023-04-28 15:03:07 +0200

    Add progress bar to icon pack downloads

--- src/web/icons/index.ts
+++ src/web/icons/index.ts
@@ -1,1 +1,40 @@
+import * as vscode from "vscode";
+
+import memoize = require("memoizee");
+
 export type IconPack = "Classic" | "Vanilla 2.1";
+
+import { download as downloadClassic } from "./classic";
+import { download as downloadVanilla } from "./vanilla";
+
+const downloadPack = async (
+	pack: IconPack,
+	progressCallback: (progress: number) => any
+) => {
+	if (pack === "Classic") {
+		return await downloadClassic(progressCallback);
+	} else if (pack === "Vanilla 2.1") {
+		return await downloadVanilla(progressCallback);
+	} else {
+		throw new Error(`Invalid icon pack name: ${pack}`);
+	}
+};
+
+const downloadPackWithProgressBar = async (pack: IconPack) => {
+	return await vscode.window.withProgress(
+		{
+			location: vscode.ProgressLocation.Notification,
+			title: `Downloading icon pack '${pack}'`,
+		},
+		async (report) => {
+			return await downloadPack(pack, (progress) => {
+				report.report({ increment: progress * 100 });
+			});
+		}
+	);
+};
+
+export const downloadIconPack: typeof downloadPackWithProgressBar = memoize(
+	downloadPackWithProgressBar,
+	{ promise: true }
+);
