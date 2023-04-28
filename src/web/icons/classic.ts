93daa868e89b31aa4af704279e01609a3e28364b
Filip Tibell <filip.tibell@gmail.com>
2023-04-28 20:28:37 +0200

    Make rest of icons work

--- src/web/icons/classic.ts
+++ src/web/icons/classic.ts
@@ -2,11 +2,16 @@ import * as path from "path";
 
 import { readZipFileAsBufferMany, readZipFileNames } from "../../utils/zip";
 import { downloadWithProgress } from "../axios";
+import { RobloxApiDump, RobloxReflectionMetadata } from "../roblox";
 
 const PACK_BASE_URL = "https://devforum.roblox.com/uploads/short-url";
 const PACK_ZIP_URL = `${PACK_BASE_URL}/vHjNEH4jUjBlz9tl8Yetp6T1b99.zip`;
 
-export const download = async (progressCallback: (progress: number) => any) => {
+export const download = async (
+	apiDump: RobloxApiDump,
+	reflection: RobloxReflectionMetadata,
+	progressCallback: (progress: number) => any
+) => {
 	const zip = await downloadWithProgress(
 		PACK_ZIP_URL,
 		(progress) => {
