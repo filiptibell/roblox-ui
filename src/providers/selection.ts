1793b3a17fecb3babb89ba699e0832ec10cf4766
Filip Tibell <filip.tibell@gmail.com>
2023-06-25 21:01:44 +0200

    Version 0.1.6

--- src/providers/selection.ts
+++ src/providers/selection.ts
@@ -10,21 +10,19 @@ export class SelectionProvider implements vscode.Disposable {
 		treeDataProvider: RojoTreeProvider
 	) {
 		this.disposables.push(
-			vscode.workspace.onDidOpenTextDocument(
-				async (document: vscode.TextDocument) => {
-					if (!treeView.visible) {
-						return;
-					}
-					let path = document.uri.fsPath;
-					if (!path.endsWith(".lua") && !path.endsWith(".luau")) {
-						return;
-					}
-					const treeItem = await treeDataProvider.findTreeItem(path);
-					if (treeItem) {
-						treeView.reveal(treeItem);
-					}
+			vscode.window.onDidChangeActiveTextEditor(async (editor) => {
+				if (!editor || !treeView.visible) {
+					return;
 				}
-			)
+				let path = editor.document.uri.fsPath;
+				if (!path.endsWith(".lua") && !path.endsWith(".luau")) {
+					return;
+				}
+				const treeItem = await treeDataProvider.findTreeItem(path);
+				if (treeItem) {
+					treeView.reveal(treeItem);
+				}
+			})
 		);
 	}
 
