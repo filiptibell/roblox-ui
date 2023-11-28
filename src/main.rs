f14a1034dff8ff98b53804324b6bc29ab80ba9b3
Filip Tibell <filip.tibell@gmail.com>
2023-11-28 11:32:51 +0100

    Use less threads for faster startup

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,11 @@ mod reflection;
 mod server;
 mod util;
 
-#[tokio::main]
+#[tokio::main(flavor = "current_thread")]
 async fn main() -> anyhow::Result<()> {
+    rayon::ThreadPoolBuilder::new()
+        .num_threads(4)
+        .build_global()
+        .expect("failed to create thread pool");
     cli::Cli::new().run().await
 }
