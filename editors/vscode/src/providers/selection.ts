477e20515ead3578df8bedd9644459c47e6d322f
Filip Tibell <filip.tibell@gmail.com>
2023-11-28 13:31:39 +0100

    Fix focus being stolen when a file is opened

--- editors/vscode/src/providers/selection.ts
+++ editors/vscode/src/providers/selection.ts
@@ -19,17 +19,32 @@ export class SelectionProvider implements vscode.Disposable {
 	}
 
 	public async revealVisibleEditors() {
+		if (!this.providers.explorerView.visible) {
+			return false;
+		}
+
+		let revealedAny = false;
 		for (const editor of vscode.window.visibleTextEditors) {
 			const fsPath = editor.document.uri.fsPath;
 			await this.providers.explorerTree.revealByPath(fsPath);
+			revealedAny = true;
 		}
+
+		return revealedAny;
 	}
 
 	public async revealActiveEditor() {
+		if (!this.providers.explorerView.visible) {
+			return false;
+		}
+
 		const activeEditor = vscode.window.activeTextEditor;
 		if (activeEditor) {
 			const fsPath = activeEditor.document.uri.fsPath;
 			await this.providers.explorerTree.revealByPath(fsPath, true);
+			return true;
 		}
+
+		return false;
 	}
 }
